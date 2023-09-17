32681224d1c819339b9054a855ae164c97265445
ArkeDemo <arke.demo.dev@gmail.com>
2023-09-17 10:20:50 +0300

    updated overview section

--- src/components/dynamicPage/index.tsx
+++ src/components/dynamicPage/index.tsx
@@ -3,6 +3,7 @@
 import { Footer } from '@/components/footer/index'
 import { Navbar } from '@/components/navbar/index'
 import { Overview } from '@/components/sections/overview/index'
+import { Overview1 } from '@/components/sections/overview-1/index'
 import { Quote } from '@/components/sections/quote/index'
 
 export const DynamicPage = (props: any) => {
@@ -26,7 +27,7 @@ export const DynamicPage = (props: any) => {
             <Navbar navbar={website.header[0]} website={website} />
             <main className="l-layout__content">
                 <div>
-                    <div className='c-divider c-divider--transparent c-divider--top' />
+                    {/* <div className='c-divider c-divider--transparent c-divider--top' /> */}
 
                     {page.content.map((section: any, i: number) => {
                         const {
@@ -35,7 +36,7 @@ export const DynamicPage = (props: any) => {
 
 
                         if (overview) {
-                            return <Overview key={i} {...section.overview} />
+                            return <Overview1 key={i} {...section.overview} />
                         }
 
                         if (quote) {
---
+++ src/components/sections/overview-1/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { RichText } from "@/components/richText";
+
+export const Overview1 = (props: any) => {
+
+  const { headline_copy, image, text_color } = props
+
+  return (
+    <>
+      <section data-testid='overview section' style={{backgroundImage: `url("${image.image.url}")`, backgroundRepeat: 'no-repeat'}}>
+        <div className="p-template-4__overview" style={{height: '70rem'}}>
+          <div className="p-template-4__overview-description" data-testid='overview description' style={{color: text_color, width: '75%'}}>
+            <RichText text={headline_copy} />
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
